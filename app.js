406f16d4382967b05e17e8195d0168090123d5a6
unknown <yehuda@itc.tech>
2022-12-05 14:31:22 +0200

    add multiple project support

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const glob = require('glob');
 const express = require("express");
 const path = require("path");
 const https = require('https');
+const G = require("glob");
 const app = express()
 const PORT = process.env.PORT || "6378"
 app.use('/', express.static(__dirname + '/tmp'))
@@ -11,34 +12,55 @@ app.use('/public', express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 
 
+
+
+
 app.get('/run', async (req, res) => {
-    if (fs.existsSync('tmp')) {
-        console.log(1);
-       await  fs.rmSync('tmp', { recursive: true, force: true });
-    }
-    let command = `git clone ${req.query.rep} tmp`
-    switch(req.query.appType) {
+    let react_port = 3333
+    let child = null
+    const rep = req.query.rep
+    const dir = req.query.rep.replace('https://', '').replace('.git', '').replaceAll('/', '_')
+    let command = `git clone ${rep} ./tmp/${dir} `
+    const appType = req.query.appType
+    switch(appType) {
         case 'react':
-            command += `&& cd tmp && npm i &&  npm start`
+            command += ` && cd ./tmp/${dir} && npm i && cross-env PORT=${react_port++} npm start`
             break;
-    }
-    console.log(command);
-    const child = exec(command, shellLog);
-    child.on('close', () => {
-        if(req.query.appType == 'react') {
-            res.redirect(`http://localhost:3000?rep=${req.query.rep}`)
-            return;
-        }
-        const files = new glob.sync('**/*.html', { dot: true });
+    }    
+    const runApp = () => {
+        if(appType === 'react') return;
+        
+        const files = new glob.sync(`./tmp/${dir}/**/*.html`, { dot: true });
         let index = getShortest(files.filter(f => f.endsWith('index.html')))
         console.log(index);
         if(!index) index = getShortest(files.filter(f => f.endsWith('main.html')))
-
-        const projectUrl = (index ? index : files[0]).replace('tmp/', '') + `?rep=${req.query.rep}`
+        
+        const projectUrl = (index ? index : files[0]).replace(`tmp/`, '') + `?rep=${req.query.rep}`
         console.log(projectUrl);
         res.redirect(`${projectUrl}`)
         // res.render('index.ejs', { projectUrl: projectUrl.replace('tmp', ''), codeRevirew: req.query.rep.replace('.git', '/pull/1/files') })
-    })
+    }
+    
+    child && await child.kill()
+
+    if(appType==="react"){
+        res.status(301).redirect(req.query.rep.replace('.git', '/pull/1/files'))
+    }
+    if(fs.existsSync(`tmp/${dir}`) && appType==="react"){
+        console.log(`cd ./tmp/${dir} && cross-env PORT=${react_port++} npm start  `);        
+        child = exec(`cd ./tmp/${dir} && cross-env PORT=${react_port++} npm start`, shellLog);    
+        return
+    }
+
+    if (!fs.existsSync(`tmp/${dir}`)){
+        console.log(command);
+        child =  exec(command, shellLog);
+        child.on('close', runApp)
+        return
+    }
+
+    runApp()
+   
 })
 
 app.get('/*', (req, res)=>{
